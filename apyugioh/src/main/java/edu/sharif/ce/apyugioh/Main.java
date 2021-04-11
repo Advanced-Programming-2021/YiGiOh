c7911d04977a7cdd7f6fc12b9656dbf78fb461f0
AlisaLC <ali_sal1381@yahoo.com>
2021-04-11 11:07:32 +0430

    removed extra files

--- apyugioh/src/main/java/edu/sharif/ce/apyugioh/Main.java
+++ apyugioh/src/main/java/edu/sharif/ce/apyugioh/Main.java
@@ -34,7 +34,6 @@ import java.util.function.Supplier;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("salam sadegh");
         ProgramController.getInstance().initialize();
         AnsiConsole.systemInstall();
         parseCommands();
