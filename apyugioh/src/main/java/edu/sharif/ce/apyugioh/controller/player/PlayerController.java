ffaba0273bd0d091bb4d2256033723857038ce65
Mohammad Moshtagh <mhmmoshtaghi@gmail.com>
2021-05-03 21:37:25 +0430

    Create PlayerController

---
+++ apyugioh/src/main/java/edu/sharif/ce/apyugioh/controller/player/PlayerController.java
@@ -0,0 +1,69 @@
+package edu.sharif.ce.apyugioh.controller.player;
+
+import edu.sharif.ce.apyugioh.model.Player;
+import lombok.Getter;
+
+@Getter
+public class PlayerController {
+    private Player player;
+
+    public PlayerController(Player player) {
+        this.player = player;
+    }
+
+    public void select() {
+
+    }
+
+    public void deselect() {
+
+    }
+
+    public void set() {
+
+    }
+
+    public void summon() {
+
+    }
+
+    public void nextPhase() {
+
+    }
+
+    public void endRound() {
+
+    }
+
+    public void startRound() {
+
+    }
+
+    public void attack(int position) {
+
+    }
+
+    public void directAttack() {
+
+    }
+
+    public void activeEffect() {
+
+    }
+
+    public void flipSummon() {
+
+    }
+
+    public void surrender() {
+
+    }
+
+    public void cancel() {
+
+    }
+
+    public void exchangeSideDeckCards() {
+
+    }
+}
