25b08581b49c1691e2d48cd0a3fc4b1808cebd2d
Mohammad Moshtagh <mhmmoshtaghi@gmail.com>
2021-05-05 05:04:32 +0430

    Add Trigger & EffectResponse
    trigger for some effects and their responses

---
+++ apyugioh/src/main/java/edu/sharif/ce/apyugioh/model/EffectResponse.java
@@ -0,0 +1,7 @@
+package edu.sharif.ce.apyugioh.model;
+
+public enum EffectResponse {
+    ATTACK_CANT_BE_DONE,
+    SUMMON_CANT_BE_DONE,
+
+}
---
+++ apyugioh/src/main/java/edu/sharif/ce/apyugioh/model/Trigger.java
@@ -0,0 +1,16 @@
+package edu.sharif.ce.apyugioh.model;
+
+public enum Trigger {
+    STANDBY,
+    DRAW,
+    SET,
+    BEFORE_SUMMON,
+    AFTER_SUMMON,
+    AFTER_FLIP_SUMMON,
+    BEFORE_ATTACK,
+    AFTER_ATTACK,
+    USER,
+    ACTIVE_SPELL,
+    ACTIVE_TRAP,
+    ALWAYS
+}
