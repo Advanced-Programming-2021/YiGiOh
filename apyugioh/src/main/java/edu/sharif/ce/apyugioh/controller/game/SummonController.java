9f61e814385825c68a021bbae1f0758635d84f98
Ali Ansari <alians310322@gmail.com>
2021-05-11 14:44:11 +0430

    Special Summon Updated

--- apyugioh/src/main/java/edu/sharif/ce/apyugioh/controller/game/SummonController.java
+++ apyugioh/src/main/java/edu/sharif/ce/apyugioh/controller/game/SummonController.java
@@ -40,6 +40,24 @@ public class SummonController {
     public boolean normalSummon() {
         if (specialCases.contains(card.getCard().getName()))
             return specialSummon();
+        if (!checkForTribute())
+            return false;
+        summon();
+        return true;
+    }
+
+    private void summon(){
+        card.setRevealed(true);
+        card.setFaceDown(false);
+        summoningPlayer.getField().removeFromHand(card);
+        summoningPlayer.getField().putInMonsterZone(card);
+        getEffectControllersByPlayer().add(new EffectController(gameControllerID,card));
+        logger.info("in game with id {}: summon successful", gameControllerID);
+        getGameController().applyEffect(Trigger.AFTER_SUMMON);
+        getGameController().applyEffect(Trigger.AFTER_NORMAL_SUMMON);
+    }
+
+    public boolean checkForTribute(){
         int availableMonsters = summoningPlayer.getField().getAvailableMonstersInZoneCount();
         if (((Monster) card.getCard()).getLevel() == 5 || ((Monster) card.getCard()).getLevel() == 6) {
             if (availableMonsters < 1) {
@@ -58,41 +76,59 @@ public class SummonController {
             if (!tribute(2))
                 return false;
         }
-        card.setRevealed(true);
-        card.setFaceDown(false);
-        summoningPlayer.getField().removeFromHand(card);
-        summoningPlayer.getField().putInMonsterZone(card);
-        getEffectControllersByPlayer().add(new EffectController(gameControllerID,card));
-        logger.info("in game with id {}: summon successful", gameControllerID);
-        getGameController().applyEffect(Trigger.AFTER_SUMMON);
-        getGameController().applyEffect(Trigger.AFTER_NORMAL_SUMMON);
         return true;
     }
 
     public boolean specialSummon(){
+        ArrayList<String> choices = new ArrayList<String>();
         switch(card.getCard().getName()){
             case "Beast King Barbaros":
-                ArrayList<String> choices = new ArrayList<String>();
                 choices.add("1. normal summon (by tributing 2 monsters)");
-                choices.add("2. normal summon (Attack points decreases to 1900");
+                choices.add("2. normal summon without tribute (Attack points decreases to 1900");
                 choices.add("3. summon by tributing 3 monsters (all cards the your rival control will be kicked out");
                 int choice = getGameController().getPlayerControllerByPlayer(summoningPlayer).chooseHowToSummon(choices);
                 if (choice == 1) {
-
+                    if (!checkForTribute())
+                        return false;
+                    summon();
+                    return true;
                 } else if (choice == 2){
-
+                    card.addAttackModifier(-1100);
+                    summon();
+                    return true;
                 } else if (choice == 3){
-
-                }
-                return true;
+                    if (!tribute(3))
+                        return
+                    getEffectControllersByPlayer().add(new EffectController(gameControllerID,card));
+                    summon();
+                    getGameController().applyEffect(Trigger.AFTER_SPECIAL_SUMMON);
+                } else
+                    return false;
             case "Gate Guardian":
                 if (summoningPlayer.getField().getAvailableMonstersInZoneCount() < 3){
                     GameController.getView().showError(GameView.ERROR_NOT_ENOUGH_CARD_TO_TRIBUTE);
                     return false;
                 }
                 if (!tribute(3))
+                    return false;
+                summon();
                 return true;
             case "The Tricky":
+                choices.add("1. summon normally (tribute 1 monster)");
+                choices.add("2. summon by removing a card from your hand");
+                int result = getGameController().getPlayerControllerByPlayer(summoningPlayer).chooseHowToSummon(choices);
+                if (result == 1){
+                    if (!checkForTribute())
+                        return false;
+                    summon();
+                    return true;
+                }else if (result == 2){
+                    GameCard selectedCardFromHand;
+                    if ((selectedCardFromHand = getGameController().getPlayerControllerByPlayer(summoningPlayer).selectCardFromHand()) != null){
+                        getGameController().removeCard(selectedCardFromHand);
+                        summon();
+                    }
+                }
                 return true;
             default:
                 normalSummon();
